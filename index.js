d64c81968ccd7b3e8a13202a639b76a43265a90f
Sebastian Rettig <serettig@posteo.de>
2018-06-25 09:15:23 +0200

    working

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 #!/usr/bin/env node
 var program = require('commander');
-var RocketChatApi = require('rocketchat').RocketChatApi;
+
+// direct access to new api
+var RocketChatClient = require('rocketchat').RocketChatClient;
 
 function login(url, username, password, port) {
   console.log('logging in to ' + url + ' as username: ' + program.username);
-  var api = new RocketChatApi('http', url, port, username, password, function(err, body) {
-		api.version(function(err,body) { console.log(body); });
-	});
+  var client = new RocketChatClient('http', url, port, username, password, function (err, body) {
+    client.authentication.me(function (err, body) {
+      console.log(body);
+      process.exit();
+    });
+  });
 }
 
 program
   .option('-u, --username <username>', 'admin user name')
   .option('-p, --password <password>', 'admin password')
-	.option('-s, --server <server>', 'server URL')
-	.option('-p, --port <port>', 'port number')
+  .option('-s, --server <server>', 'server URL')
+  .option('-o, --port <port>', 'port number')
   .command('addusers <nr>')
   .action(function (nr, cmd) {
-  	console.log('adding ' + nr + ' users');
-		login(program.server, program.username, program.password, parseInt(program.port));
+    console.log('adding ' + nr + ' users');
+    login(program.server, program.username, program.password, parseInt(program.port));
   });
 
 program.parse(process.argv);
