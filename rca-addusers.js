19f2048705d5d87678cd878c4b7c8efd36705186
Sebastian Rettig <serettig@posteo.de>
2018-06-25 10:25:25 +0200

    added concurrency in adding users

--- rca-addusers.js
+++ rca-addusers.js
@@ -1,14 +1,40 @@
 var program = require('commander');
 var common = require('./common');
+var PromisePool = require('es6-promise-pool')
+
+var currentNr = 0;
+var globalClient;
+var stopNr;
+
+var promiseProducer = function () {
+  if (currentNr < stopNr) {
+    currentNr++;
+    console.log("Creating promise for " + "username" + currentNr + " stopping @ " + stopNr);
+    return common.createUser(globalClient, "username" + currentNr);    
+  }
+  else {
+    return null;
+  }
+}
 
 async function addUsers(url, username, password, port, nr) {
   console.log('logging in to ' + url + ' as ' + username);
   let client = await common.login(url, username, password, port);
-  for (let counter = 0; counter < nr; counter++) {
-    let ret = await common.createUser(client, "username" + counter);
-    console.log("Created user " + "username" + counter);
-  }
-  process.exit();
+
+  globalClient = client;
+  currentNr = 0;
+  stopNr = nr;
+
+  var pool = new PromisePool(promiseProducer, 5)
+  var poolPromise = pool.start();
+  poolPromise.then(function () {
+    console.log("all done!");
+    process.exit();
+  }, function (error) {
+    console.log("there was an error: " + error.message);
+    process.exit();
+  })
+
 }
 
 program
